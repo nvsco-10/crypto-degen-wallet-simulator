d83d3d46482ee256f7c4937646643d977d96d1ae
Neema Velasco <neemavelasco@gmail.com>
2022-10-10 14:03:12 -0500

    added wallet-header component layout and styles

---
+++ src/components/wallet-header/wallet-header.component.jsx
@@ -0,0 +1,43 @@
+import { WalletHeaderContainer } from "./wallet-header.styles"
+import { CgOptions } from "react-icons/cg"
+import { BiPlus, BiMinus, BiTransferAlt } from "react-icons/bi"
+
+export const WalletHeader = () => {
+  return (
+    <WalletHeaderContainer>
+      <div className="settings-container">
+        <span className="icon-btn">
+          <CgOptions className="icon" />
+        </span>
+      </div>
+      <div className="total-balance-container">
+        <span className="total-balance">$5370</span>
+        <p>My Wallet</p>
+      </div>
+      <div className="btns-container">
+
+        <div className="btn-container">
+          <div className="icon-btn">
+            <BiPlus className="icon" />
+          </div>
+          <p>BUY</p>
+        </div>
+
+        <div className="btn-container">
+          <div className="icon-btn">
+            <BiMinus className="icon" />
+          </div>
+          <p>SELL</p>
+        </div>
+
+        <div className="btn-container">
+          <div className="icon-btn">
+            <BiTransferAlt className="icon" />
+          </div>
+          <p>TRANSFER</p>
+        </div>
+
+      </div>
+    </WalletHeaderContainer>
+  )
+}
---
+++ src/components/wallet-header/wallet-header.styles.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const WalletHeaderContainer = styled.div`
+  height: 20rem;
+  padding: 0.75em 0;
+  /* border: solid 1px white; */
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .settings-container {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 2rem;
+
+    .icon {
+      font-size: 2rem;
+    }
+  }
+
+  .total-balance-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .total-balance {
+      font-size: 2.4rem;
+    }
+  }
+
+  .btns-container {
+    display: flex;
+    gap: 2.5rem;
+    margin: 3rem 0 0 0;
+
+    .btn-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .icon-btn {
+      margin-bottom: .75rem;
+      width: 55px;
+      background-color: var(--dark-blue);
+      border-radius: 8%;
+    }
+
+    .icon {
+      font-size: 2.7rem;
+    }
+
+    p {
+      font-size: var(--text-xsmall);
+    }
+  }
+
+`
